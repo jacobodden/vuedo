98b670ab893e8a2c03088f55edf499737f8b06f6
Jacob Odden <jacob.l.odden@gmail.com>
2018-01-02 20:42:27 -0500

    Add routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Todos from '@/components/Todos'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/todos',
+      name: 'Todos',
+      component: Todos
     }
   ]
 })
